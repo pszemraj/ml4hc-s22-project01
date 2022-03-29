2c11aecdb93a1e86ecceb129629017e3bf116981
Peter <74869040+pszemraj@users.noreply.github.com>
2022-03-29 09:08:27 +0200

    :art: format to black

--- notebooks/analysis/merge_compare_results.py
+++ notebooks/analysis/merge_compare_results.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 import pprint as pp
 
-_root = Path(__file__).parent.parent.parent # two levels up
+_root = Path(__file__).parent.parent.parent  # two levels up
 results_dir = _root / "results"
 _results_subdirs = [d for d in results_dir.iterdir() if d.is_dir()]
 result_dirs = {d.name: d for d in _results_subdirs}
@@ -22,34 +22,39 @@ pp.pprint(list(result_dirs.keys()))
 # LE GRAND MAPPING DICTIONNARY
 
 metric_map = {
-    'F1': 'f1_score',
-    'Accuracy': 'accuracy',
-    'MCC': 'matthews_corrcoef',
-    'Balanced Accuracy': 'balanced_accuracy_score',
-    'AUC': 'roc_auc_score',
-    'Prec.': 'precision_score',
-    'Recall': 'recall_score',
-    'Kappa': 'cohen_kappa_score',
-
+    "F1": "f1_score",
+    "Accuracy": "accuracy",
+    "MCC": "matthews_corrcoef",
+    "Balanced Accuracy": "balanced_accuracy_score",
+    "AUC": "roc_auc_score",
+    "Prec.": "precision_score",
+    "Recall": "recall_score",
+    "Kappa": "cohen_kappa_score",
 }
 # %%
 
 # handle initial-training
 key_phrase = "model_metrics"
 # load CSV paths in initial-training directory that contain the key phrase
-_csv_paths = [f for f in result_dirs["initial-training"].iterdir() if f.is_file() and key_phrase in f.name.lower()]
+_csv_paths = [
+    f
+    for f in result_dirs["initial-training"].iterdir()
+    if f.is_file() and key_phrase in f.name.lower()
+]
 print(f"{len(_csv_paths)} CSV files found in {result_dirs['initial-training']}")
 # %%
 
 # load all CSV files as dataframes and print the column names
 
-def get_unique_cols(csv_paths:list):
+
+def get_unique_cols(csv_paths: list):
 
     _dfs = [pd.read_csv(f) for f in csv_paths]
     _cols = [list(df.columns) for df in _dfs]
     # find the unique column names between all files and dataframes
     _cols = set.intersection(*map(set, _cols))
     return _cols
+
 
 get_unique_cols(_csv_paths)
 
@@ -68,14 +73,21 @@ for f in _csv_paths:
     _cols = list(df.columns)
     _cols.remove("model_filename")
     # all column entries except for the model file name now become one column titled performance_metric using pd.melt
-    df = pd.melt(df, id_vars=["model_filename"], value_vars=_cols, var_name="performance_metric", value_name="metric_value")
-    df['dataset'] = "mitbih" if "mitbih" in f.name.lower() else "ptbdb"
+    df = pd.melt(
+        df,
+        id_vars=["model_filename"],
+        value_vars=_cols,
+        var_name="performance_metric",
+        value_name="metric_value",
+    )
+    df["dataset"] = "mitbih" if "mitbih" in f.name.lower() else "ptbdb"
     overall_results = pd.concat([overall_results, df], axis=0)
 
 # %%
 # do a bit more cleaning
 
-def remove_substring(text: str, substring: str=None):
+
+def remove_substring(text: str, substring: str = None):
     """
     remove_substring - removes a substring from a string
 
@@ -91,9 +103,12 @@ def remove_substring(text: str, substring: str=None):
     substring = substring if substring is not None else "_weights_preds"
     return text.replace(substring, "")
 
-overall_results["model_filename"] = overall_results["model_filename"].apply(remove_substring)
+
+overall_results["model_filename"] = overall_results["model_filename"].apply(
+    remove_substring
+)
 overall_results.reset_index(drop=True, inplace=True)
-overall_results['source'] = "initial-training"
+overall_results["source"] = "initial-training"
 overall_results.head()
 
 # %%
@@ -101,7 +116,9 @@ overall_results.head()
 key_phrase = "fit_search"
 # load CSV paths in initial-training directory that contain the key phrase
 _ensemble_root = result_dirs["ensembling-models"]
-_csv_paths = [f for f in _ensemble_root.iterdir() if f.is_file() and key_phrase in f.name.lower()]
+_csv_paths = [
+    f for f in _ensemble_root.iterdir() if f.is_file() and key_phrase in f.name.lower()
+]
 print(f"{len(_csv_paths)} CSV files found in {_ensemble_root.name}")
 
 # %%
@@ -114,7 +131,8 @@ if len(not_in_map) > 0:
     print(f"{len(not_in_map)} columns not in map: {not_in_map}")
 # %%
 
-def check_mapping(col_name: str, mapping: dict, exclude_list: list=None):
+
+def check_mapping(col_name: str, mapping: dict, exclude_list: list = None):
     """
     check_mapping - checks if a column name is in the mapping dictionary
 
@@ -129,19 +147,22 @@ def check_mapping(col_name: str, mapping: dict, exclude_list: list=None):
     """
     if col_name in exclude_list:
         return col_name
-    if col_name in mapping.keys() :
+    if col_name in mapping.keys():
         return mapping[col_name]
     else:
         return False
 
+
 for f in _csv_paths:
-    df = pd.read_csv(f)
+    df = pd.read_csv(f).convert_dtypes()
     _cols = list(df.columns)
-    # replace the column name Model by 'model_filename'
-    _cols = [c if c != "Model" else "model_filename" for c in _cols]
+    _cols = [
+        c if c != "Model" else "model_filename" for c in _cols
+    ]  # replace the column name Model by 'model_filename'
     df.columns = _cols
     new_cols = []
     for colname in list(df.columns):
+        # check if the column name is in the mapping dictionary, if not, delete the column
         std_name = check_mapping(colname, metric_map, exclude_list=["model_filename"])
         if not std_name:
             del df[colname]
@@ -152,6 +173,19 @@ for f in _csv_paths:
     _cols = list(df.columns)
     _cols.remove("model_filename")
     # all column entries except for the model file name now become one column titled performance_metric using pd.melt
-    df = pd.melt(df, id_vars=["model_filename"], value_vars=_cols, var_name="performance_metric", value_name="metric_value")
-    df['dataset'] = "mitbih" if "mitbih" in f.name.lower() else "ptbdb"
+    df = pd.melt(
+        df,
+        id_vars=["model_filename"],
+        value_vars=_cols,
+        var_name="performance_metric",
+        value_name="metric_value",
+    )
+    df["dataset"] = "mitbih" if "mitbih" in f.name.lower() else "ptbdb"
+    df["source"] = "ensembling-models"
     overall_results = pd.concat([overall_results, df], axis=0)
+
+
+overall_results.info()
+# %%
+overall_results.value_counts()
+# %%
